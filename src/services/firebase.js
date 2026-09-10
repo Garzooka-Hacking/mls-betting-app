5af29fd1dd53e9249b277a62370d51b308039616
pachotadas27-gif <pachotadas27@gmail.com>
2026-09-10 13:10:25 -0500

    Fix API Key typo

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,7 +3,7 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDzObXgfQwA70XMtUDVbZthKfr5t62Y17U",
+  apiKey: "AIzaSyDz0bXgfQwA70XMtUDVbZthKfr5t62Y17U",
   authDomain: "mls-betting-app.firebaseapp.com",
   projectId: "mls-betting-app",
   storageBucket: "mls-betting-app.firebasestorage.app",
